b3b3213fe215e4a7845c434de2a231668a71ca71
beitangshin <xialiaqia@gmail.com>
2019-09-23 21:33:34 +0200

    Update lab1.adb

--- lab1.adb
+++ lab1.adb
@@ -9,11 +9,12 @@ procedure Cyclic is
    D: Duration := 1.0;
    Start_Time: Time := Clock;
    S: Integer := 0;
-   
+   T: Time := Clock;
    
    procedure F1 is 
       Message: constant String := "f1 executing, time is now";
    begin
+      T := Clock;
       Put(Message);
       Put_Line(Duration'Image(Clock - Start_Time));
    end F1;
@@ -21,8 +22,10 @@ procedure Cyclic is
    procedure F2 is 
       Message: constant String := "f2 executing, time is now";
    begin
+     -- T2: Time := Clock;	  
       Put(Message);
       Put_Line(Duration'Image(Clock - Start_Time));
+     -- TD : Duration := T2+T2+T1;
    end F2;
    
    procedure F3 is 
@@ -37,7 +40,11 @@ begin
       -- change/add your code inside this loop   
       F1;
       F2;
+      delay until T+0.5;
       F3;
-      delay D;
+      delay until T+1.0;
+      F1;
+      F2;
+      delay until T+1.0;
    end loop;
    end Cyclic;
